db9ac64fbacfcfd44c4f7bb78172aacdc0bc3f1f
falconnine9 <63831264+falconnine9@users.noreply.github.com>
2021-10-01 08:31:59 -0600

    Towers should work now

--- src/structures/tower.js
+++ src/structures/tower.js
@@ -1,1 +1,40 @@
-// Placeholder
+module.exports.allActions = (room) => {
+    for (const tower of room.find(FIND_MY_STRUCTURES, {
+        filter: s => s.structureType === STRUCTURE_TOWER
+    })) {
+        doActions(tower);
+    }
+}
+
+
+function doActions(tower) {
+    const hostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+    if (hostile) {
+        const status = tower.attack(hostile);
+        if (status === ERR_NOT_ENOUGH_ENERGY) {
+            tower.memory.needsEnergy = true;
+        }
+        else {
+            if (tower.store.getUsedCapacity() === 0) {
+                tower.memory.needsEnergy = true;
+            }
+        }
+    }
+
+    else {
+        const friendly = tower.pos.findClosestByRange(FIND_MY_CREEPS, {
+            filter: c => c.hits < c.hitsMax
+        });
+        if (friendly) {
+            const status = tower.heal(friendly);
+            if (status === ERR_NOT_ENOUGH_ENERGY) {
+                tower.memory.needsEnergy = true;
+            }
+            else {
+                if (tower.store.getUsedCapacity() === 0) {
+                    tower.memory.needsEnergy = true;
+                }
+            }
+        }
+    }
+}
