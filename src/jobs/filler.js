9014829c05f7a16df33b48926de926a9901d0b11
falconnine9 <63831264+falconnine9@users.noreply.github.com>
2021-10-02 09:55:50 -0600

    Make fillers fill up spawn/extensions

--- src/jobs/filler.js
+++ src/jobs/filler.js
@@ -20,31 +20,69 @@ function doActions(creep) {
             }
         }
     });
-    if (structs.length === 0) return;
-    structs.sort((a, b) => {
-        const aDiff = a.store.getCapacity(RESOURCE_ENERGY) - a.store.getUsedCapacity(RESOURCE_ENERGY);
-        const bDiff = b.store.getCapacity(RESOURCE_ENERGY) - b.store.getUsedCapacity(RESOURCE_ENERGY);
-        return aDiff - bDiff;
-    });
+    if (structs.length > 0) {
+        structs.sort((a, b) => {
+            const aDiff = a.store.getCapacity(RESOURCE_ENERGY) - a.store.getUsedCapacity(RESOURCE_ENERGY);
+            const bDiff = b.store.getCapacity(RESOURCE_ENERGY) - b.store.getUsedCapacity(RESOURCE_ENERGY);
+            return aDiff - bDiff;
+        });
 
-    if (creep.store.getFreeCapacity() === 0) {
-        const status = creep.transfer(structs[0], RESOURCE_ENERGY);
-        if (status === ERR_NOT_IN_RANGE) {
-            creep.moveTo(structs[0]);
+        if (creep.store.getFreeCapacity(RESOURCE_ENERGY) === 0) {
+            const status = creep.transfer(structs[0], RESOURCE_ENERGY);
+            if (status === ERR_NOT_IN_RANGE) {
+                creep.moveTo(structs[0]);
+            }
+        }
+        else {
+            const container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                filter: s => s.structureType === STRUCTURE_STORAGE
+            });
+
+            if (container) {
+                const status = creep.withdraw(container, RESOURCE_ENERGY, creep.store.getFreeCapacity(RESOURCE_ENERGY));
+                if (status === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(container);
+                }
+                else if (status === ERR_NOT_ENOUGH_RESOURCES) {
+                    creep.withdraw(container, RESOURCE_ENERGY, spawn.store.getUsedCapacity(RESOURCE_ENERGY));
+                }
+            }
         }
     }
+
     else {
+        const spawn = creep.pos.findClosestByRange(FIND_MY_SPAWNS);
         const container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
             filter: s => s.structureType === STRUCTURE_STORAGE
         });
+        if (!container) return;
 
-        if (container) {
+        if (creep.store.getFreeCapacity(RESOURCE_ENERGY) === 0) {
+            if (spawn.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
+                const status = creep.transfer(spawn, RESOURCE_ENERGY);
+                if (status === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(spawn);
+                }
+            }
+            else {
+                const extension = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                    filter: s => s.structureType === STRUCTURE_EXTENSION && s.store.getFreeCapacity(RESOURCE_ENERGY) > 0
+                });
+                if (extension) {
+                    const status = creep.transfer(extension, RESOURCE_ENERGY);
+                    if (status === ERR_NOT_IN_RANGE) {
+                        creep.moveTo(extension);
+                    }
+                }
+            }
+        }
+        else {
             const status = creep.withdraw(container, RESOURCE_ENERGY, creep.store.getFreeCapacity(RESOURCE_ENERGY));
             if (status === ERR_NOT_IN_RANGE) {
                 creep.moveTo(container);
             }
             else if (status === ERR_NOT_ENOUGH_RESOURCES) {
-                creep.withdraw(container, RESOURCE_ENERGY, spawn.store.getUsedCapacity(RESOURCE_ENERGY));
+                creep.withdraw(container, RESOURCE_ENERGY, container.store.getUsedCapacity(RESOURCE_ENERGY));
             }
         }
     }
