7da6ca1ee1ecc4e3ac268e733631e0537745be66
falconnine9 <63831264+falconnine9@users.noreply.github.com>
2021-10-03 17:03:25 -0600

    Add remote building functionality

--- src/jobs/builder.js
+++ src/jobs/builder.js
@@ -18,7 +18,7 @@ function doActions(creep) {
         config = allConfig["global"];
     }
 
-    if (sites.length > 0) {
+    if (sites.length > 0 && creep.room.name === creep.memory.origin) {
         sites.sort((a, b) => (a.progressTotal - a.progress) - (b.progressTotal - b.progress));
 
         if (creep.store.getUsedCapacity(RESOURCE_ENERGY) === 0) {
@@ -44,6 +44,38 @@ function doActions(creep) {
             const status = creep.build(sites[0]);
             if (status === ERR_NOT_IN_RANGE) {
                 creep.moveTo(sites[0]);
+            }
+        }
+    }
+
+    else {
+        let remoteSite;
+        for (const roomName in Game.rooms) {
+            const roomIter = Game.rooms[roomName];
+            const iterSites = roomIter.find(FIND_MY_CONSTRUCTION_SITES, {
+                filter: s => s.structureType === STRUCTURE_SPAWN
+            });
+            if (iterSites.length > 0) {
+                remoteSite = iterSites[0];
+                break;
+            }
+        }
+        if (!remoteSite) return;
+        const home = Game.rooms[creep.memory.origin];
+
+        if (creep.store.getUsedCapacity(RESOURCE_ENERGY) === 0) {
+            const container = home.storage;
+            if (container) {
+                const status = creep.withdraw(container, RESOURCE_ENERGY);
+                if (status === ERR_NOT_IN_RANGE) {
+                    creep.moveTo(container);
+                }
+            } else return;
+        }
+        else {
+            const status = creep.build(remoteSite);
+            if (status === ERR_NOT_IN_RANGE) {
+                creep.moveTo(remoteSite);
             }
         }
     }
