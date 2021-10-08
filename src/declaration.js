9e392cf0459585b09a685813f6bd791c1da973c1
falconnine9 <63831264+falconnine9@users.noreply.github.com>
2021-10-08 11:38:39 -0600

    Old code go die

--- src/declaration.js
+++
@@ -1,7 +0,0 @@
-function injectMethods() {
-    Creep.prototype.wander = () => {
-        if (!this.fatigue) return;
-        const direction = Math.floor((Math.random() * 8) + 1);
-        this.move(direction);
-    }
-}
